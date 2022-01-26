92f2836fbab776b082244b368d4b846b060138dd
jorge <jocaro1998@hotmail.com>
2022-01-26 17:00:52 +0100

    tercer commit

--- src/com/nttdata/PruebaGit.java
+++ src/com/nttdata/PruebaGit.java
@@ -9,4 +9,57 @@ public class PruebaGit {
 		System.out.println("Hola, soy Jorge");
 	}
 
+//	
+//	jocar@DESKTOP-RBQQN07 MINGW64 ~/eclipse-workspace/NTTDataGitJCR
+//	$ git init .
+//	Initialized empty Git repository in C:/Users/jocar/eclipse-workspace/NTTDataGitJCR/.git/
+//
+//	jocar@DESKTOP-RBQQN07 MINGW64 ~/eclipse-workspace/NTTDataGitJCR (master)
+//	$ git add --all
+//
+//	jocar@DESKTOP-RBQQN07 MINGW64 ~/eclipse-workspace/NTTDataGitJCR (master)
+//	$ git commit -m "primer commit"
+//	[master (root-commit) 480019c] primer commit
+//	 5 files changed, 52 insertions(+)
+//	 create mode 100644 .classpath
+//	 create mode 100644 .project
+//	 create mode 100644 .settings/org.eclipse.jdt.core.prefs
+//	 create mode 100644 bin/com/nttdata/PruebaGit.class
+//	 create mode 100644 src/com/nttdata/PruebaGit.java
+//
+//	jocar@DESKTOP-RBQQN07 MINGW64 ~/eclipse-workspace/NTTDataGitJCR (master)
+//	$ git init .
+//	Reinitialized existing Git repository in C:/Users/jocar/eclipse-workspace/NTTDataGitJCR/.git/
+//
+//	jocar@DESKTOP-RBQQN07 MINGW64 ~/eclipse-workspace/NTTDataGitJCR (master)
+//	$ git add --all
+//
+//	jocar@DESKTOP-RBQQN07 MINGW64 ~/eclipse-workspace/NTTDataGitJCR (master)
+//	$ git commit -m "segundo commit"
+//	[master c8ffbff] segundo commit
+//	 2 files changed, 1 insertion(+)
+//
+//	jocar@DESKTOP-RBQQN07 MINGW64 ~/eclipse-workspace/NTTDataGitJCR (master)
+//	$ git status -s
+//
+//	jocar@DESKTOP-RBQQN07 MINGW64 ~/eclipse-workspace/NTTDataGitJCR (master)
+//	$ git status -s
+//
+//	jocar@DESKTOP-RBQQN07 MINGW64 ~/eclipse-workspace/NTTDataGitJCR (master)
+//	$ git log
+//	commit c8ffbffef541d50ef5dbb5c4f29e00c5f5a9be99 (HEAD -> master)
+//	Author: jorge <jocaro1998@hotmail.com>
+//	Date:   Wed Jan 26 16:47:15 2022 +0100
+//
+//	    segundo commit
+//
+//	commit 480019c7eda2144ca0def220bd881a8e4a834159
+//	Author: jorge <jocaro1998@hotmail.com>
+//	Date:   Wed Jan 26 16:44:36 2022 +0100
+//
+//	    primer commit
+//
+//	jocar@DESKTOP-RBQQN07 MINGW64 ~/eclipse-workspace/NTTDataGitJCR (master)
+//	$
+
 }
