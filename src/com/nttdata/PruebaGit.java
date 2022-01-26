480019c7eda2144ca0def220bd881a8e4a834159
jorge <jocaro1998@hotmail.com>
2022-01-26 16:44:36 +0100

    primer commit

---
+++ src/com/nttdata/PruebaGit.java
@@ -0,0 +1,11 @@
+package com.nttdata;
+
+public class PruebaGit {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hola, mundo");
+	}
+
+}
